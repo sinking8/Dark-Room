2819127be29d84e0bf5c2a5cbbfae50d4c53d192
sinking8 <ashwinblaze111@gmail.com>
2023-11-05 11:47:43 -0800

    up

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,10 @@
-import logo from './logo.png';
-import home from './imagens/home.png';
-import mensagem from './imagens/mensagem.png';
-import adicionar from './imagens/adicionar.png';
-import bussola from './imagens/bussola.png';
-import coracao from './imagens/coracao.png';
-import perfil from './imagens/perfil.jpg';
+import logo from "./logo.jpg";
+import home from "./imagens/home.png";
+import mensagem from "./imagens/mensagem.png";
+import adicionar from "./imagens/adicionar.png";
+import bussola from "./imagens/bussola.png";
+import coracao from "./imagens/coracao.png";
+import perfil from "./imagens/perfil.jpg";
 
 function Header() {
   return (
@@ -12,26 +12,34 @@ function Header() {
       <header>
         <div className="header-border">
           <div className="container-header">
-              <div>
-                <img src={logo} className="logo" style={{width:"53px",height:"50px",paddingBottom:"10px"}} />
-              </div>       
+            <div>
+              <img
+                src={logo}
+                className="logo"
+                style={{ width: "60px", height: "50px" }}
+              />
+            </div>
 
-              <div className="barra">
-                <input type="text" className="pesquisa" placeholder="Search"></input>
+            <div className="barra" style={{ marginBottom: "10px" }}>
+              <input
+                type="text"
+                className="pesquisa"
+                placeholder="Search"
+              ></input>
+            </div>
+
+            <nav>
+              <div className="links" style={{ marginBottom: "10px" }}>
+                <img src={home} className="home" alt="home" />
+                <img src={mensagem} className="mensagem" alt="mensagem" />
+                <img src={adicionar} className="adicionar" alt="adicionar" />
+                <img src={bussola} className="bussola" alt="bussola" />
+                <img src={coracao} className="coracao" alt="coração" />
+                <img src={perfil} className="perfil" alt="perfil" />
               </div>
-
-              <nav>
-                <div className="links">
-                  <img src={home} className="home" alt="home" />
-                  <img src={mensagem} className="mensagem" alt="mensagem" />
-                  <img src={adicionar} className="adicionar" alt="adicionar" />
-                  <img src={bussola} className="bussola" alt="bussola" />
-                  <img src={coracao} className="coracao" alt="coração" />
-                  <img src={perfil} className="perfil" alt="perfil" />
-                </div>
-              </nav>
+            </nav>
           </div>
-        </div>        
+        </div>
       </header>
     </>
   );
--- src/components/perfil.js
+++ src/components/perfil.js
@@ -1,119 +1,123 @@
-import perfil from './imagens/perfil.jpg';
-import perfil2 from './imagens/perfil2.jpg';
-import perfil3 from './imagens/perfil3.jpg';
-import perfil4 from './imagens/perfil4.jpg';
-import perfil5 from './imagens/perfil5.jpg';
-
+import perfil from "./imagens/perfil.jpg";
+import perfil2 from "./imagens/perfil2.jpg";
+import perfil3 from "./imagens/perfil3.jpg";
+import perfil4 from "./imagens/perfil4.jpg";
+import perfil5 from "./imagens/perfil5.jpg";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
 function Perfil() {
   return (
     <>
       <div className="informacoes">
-            <div className="container-informacoes">
-                <div className="mudar-perfil">
-                    <div className="perfil-informacoes">
-                        <div>
-                            <img src={perfil} className="foto-informacoes" alt="perfil" />
-                            <div className="text-perfil">
-                                <h1>John</h1>
-                                <p>John</p>
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <p className="mudar">+</p>
-                        </div>                            
-                    </div>                          
+        <div className="container-informacoes">
+          <div className="mudar-perfil">
+            <div className="perfil-informacoes">
+              <div>
+                <img src={perfil} className="foto-informacoes" alt="perfil" />
+                <div className="text-perfil">
+                  <h1>John</h1>
                 </div>
+              </div>
 
-                <div className="sugestoes">
-                    <div className="sugestoes-texto">
-                            <h1>Suggestions</h1>
-                    </div>
-
-                    <div className="sugestoes-container">
-                        <div className="perfil-sugestoes">
-                            <div>
-                                <img src={perfil} className="foto-sugestoes" alt="perfil" />
-                                <div className="text-perfil2">
-                                    <h1>emily</h1>
-                                    <p>user name</p>
-                                </div>
-                            </div>
-                            
-                            <div>
-                                <p className="follow">follow</p>
-                            </div>                            
-                        </div>                        
-                    </div>
-
-                    <div className="sugestoes-container">
-                        <div className="perfil-sugestoes">
-                            <div>
-                                <img src={perfil2} className="foto-sugestoes" alt="perfil" />
-                                <div className="text-perfil2">
-                                    <h1>jacob</h1>
-                                    <p>user name2</p>
-                                </div>
-                            </div>
-                            
-                            <div>
-                                <p className="follow">follow</p>
-                            </div>                            
-                        </div>  
-                    </div>
-
-                    <div className="sugestoes-container">
-                        <div className="perfil-sugestoes">
-                            <div>
-                                <img src={perfil3} className="foto-sugestoes" alt="perfil" />
-                                <div className="text-perfil2">
-                                    <h1>Ashwin</h1>
-                                    <p>user name2</p>
-                                </div>
-                            </div>
-                            
-                            <div>
-                                <p className="follow">follow</p>
-                            </div>                            
-                        </div>  
-                    </div>
-
-                    <div className="sugestoes-container">
-                        <div className="perfil-sugestoes">
-                            <div>
-                                <img src={perfil5} className="foto-sugestoes" alt="perfil" />
-                                <div className="text-perfil2">
-                                    <h1>Ashwin</h1>
-                                    <p>user name2</p>
-                                </div>
-                            </div>
-                            
-                            <div>
-                                <p className="follow">follow</p>
-                            </div>                            
-                        </div>  
-                    </div>
-
-                    <div className="sugestoes-container">
-                        <div className="perfil-sugestoes">
-                            <div>
-                                <img src={perfil4} className="foto-sugestoes" alt="perfil" />
-                                <div className="text-perfil2">
-                                    <h1>JK</h1>
-                                    <p>user name2</p>
-                                </div>
-                            </div>
-                            
-                            <div>
-                                <p className="follow">follow</p>
-                            </div>                            
-                        </div>  
-                    </div>
-                </div>
-
-                <div className="footer">
-                </div>
+              <div>
+                <p className="mudar">+</p>
+              </div>
             </div>
+          </div>
+
+          <div className="sugestoes">
+            <div className="sugestoes-texto">
+              <h1>Team Details</h1>
+            </div>
+
+            <div className="sugestoes-container">
+              <div className="perfil-sugestoes">
+                <div>
+                  <img src={perfil} className="foto-sugestoes" alt="perfil" />
+                  <div className="text-perfil2">
+                    <h1>Ashwin</h1>
+                    <p>Cityzen</p>
+                  </div>
+                </div>
+
+                <div>
+                  <p className="follow">follow</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="sugestoes-container">
+              <div className="perfil-sugestoes">
+                <div>
+                  <img src={perfil2} className="foto-sugestoes" alt="perfil" />
+                  <div className="text-perfil2">
+                    <h1>Nithya</h1>
+                    <p>Part time farmer</p>
+                  </div>
+                </div>
+
+                <div>
+                  <p className="follow">follow</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="sugestoes-container">
+              <div className="perfil-sugestoes">
+                <div>
+                  <img src={perfil3} className="foto-sugestoes" alt="perfil" />
+                  <div className="text-perfil2">
+                    <h1>Sudeeksha</h1>
+                    <p>data Storyteller</p>
+                  </div>
+                </div>
+
+                <div>
+                  <p className="follow">follow</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="sugestoes-container">
+              <div className="perfil-sugestoes">
+                <div>
+                  <img src={perfil5} className="foto-sugestoes" alt="perfil" />
+                  <div className="text-perfil2">
+                    <h1>JayKrith</h1>
+                    <p>XOTWOD</p>
+                  </div>
+                </div>
+
+                <div>
+                  <p className="follow">follow</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="footer"></div>
+        </div>
+        <br></br>
+
+        <div>
+          <h4 style={{ paddingRight: "350px" }}>About Us</h4>
+          <Card style={{ marginTop: "10px" }}>
+            <CardContent>
+              <p align="justify" style={{ color: "black" }}>
+                {" "}
+                Dark Room is an AI-driven product in social media and video
+                streaming arenas with a mission to enable community and
+                entertainment reach to the visually impaired sections of the
+                population. Dark Room provides audio descriptions for posts and
+                videos. We help visually impaired to stay connected, build
+                relations, access information on world events and find
+                opportunities.
+              </p>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </>
   );
